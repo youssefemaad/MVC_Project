640732330c0e2d3b11181438b95ad3c82ba69e6e
youssefemaad <eyoussef228@gmail.com>
2025-04-04 02:41:42 +0200

    Employee Module - Create Action

--- Demo.Presentation/Controllers/EmployeeController.cs
+++ Demo.Presentation/Controllers/EmployeeController.cs
@@ -1,10 +1,16 @@
+using Demo.BusinessLogic.DataTransferObjects.DepartmentDataTransfer;
+using Demo.BusinessLogic.DataTransferObjects.EmployeeDataTransfer;
+using Demo.BusinessLogic.Services.Classes;
 using Demo.BusinessLogic.Services.Interface;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeeController(IEmployeeService employeeService) : Controller
+    public class EmployeeController(IEmployeeService employeeService
+        , ILogger<DepartmentsController> _logger,
+        IWebHostEnvironment environment) : Controller
     {
         public IActionResult Index()
         {
@@ -13,15 +19,36 @@ namespace Demo.Presentation.Controllers
         }
 
         #region Create Employee
-
-        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                try
+                {
+                    int Result = employeeService.CreateEmployee(employeeDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Can't Create Employee");
+                    }
+                }
+                catch (Exception ex)
+                {
 
-       
-
+                    if (environment.IsDevelopment())
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    else
+                        _logger.LogError(ex.Message);
+                }
+            }
+            return View(employeeDto);
+        }
         #endregion
     }
 }
